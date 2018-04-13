cb4f4c141d3f755925f80bb0e2fe5f5daacb95f7
Noah <williamson.noah@gmail.com>
2018-04-13 16:00:19 -0400

    first commit, added file skeleton

---
+++ chat_server.c
@@ -0,0 +1,16 @@
+/*
+ * chat_server.c
+ *
+ * Author: Noah Williamson
+ * Course: CS371
+ * Project 2
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+  return 0;
+}
+
